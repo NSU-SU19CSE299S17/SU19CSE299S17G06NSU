574aa7f9d06ad74560af68c15b0ff6cd34f08147
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-06-26 21:26:37 +0600

    Query list was created

--- Project Code/homepage/index.php
+++ Project Code/homepage/index.php
@@ -10,10 +10,14 @@
 		<div class="logo">
 		<img src="image/h.png">
 		</div>
-
-
-
-
+		<ul class="main-nav">
+			<li class="active"> <a href=""> HOME </a> </li>
+			<li> <a href="Service.php"> SERVICE </a> </li>
+			<li> <a href="about.php"> ABOUT </a> </li>
+			<li> <a href="Contact.php"> CONTACT </a> </li>
+			<li> <a href="faq.php"> FAQ </a> </li>
+		
+		</ul>
 
 </header>
 
