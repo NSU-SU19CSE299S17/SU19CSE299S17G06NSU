755552a85eb3ce9cdff775184ab172aa073638a4
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-06-26 21:23:16 +0600

    logo and basic frame was created

---
+++ Project Code/homepage/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Furniture Shop</title>
+<link href="style.css" rel="stylesheet" type="text/css"
+</head>
+<body>
+<header>
+<div class="row">
+		<div class="logo">
+		<img src="image/h.png">
+		</div>
+
+
+
+
+
+</header>
+
+
+
+
+</body>
+</html>
