1a38b79fa14545ba7157f9a8be76122f37573c32
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-03 22:58:32 +0600

    issues #13 about page created

---
+++ Project Code/homepage/about.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+<header>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<p> The E-Furniture shop com has been found in 2019 with the vision to make online shopping easier with top notch customer service.
+Now a days million of users use this marketplace to buy or sell their product everyday.</p>
+
+</header>
+</head>
+<body>
+
+
+
+</body>
+</html>
