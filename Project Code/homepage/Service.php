77269649ce2515cdd85eacbc70b8b802349b5ae9
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-04 22:33:43 +0600

    Create Service.php
    issues #13 service page added

---
+++ Project Code/homepage/Service.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body>
+<style>
+
+<p>
+<ul>
+<li> Fast Order </li>
+<li> Fast Delivery </li>
+<li> Order taken from any region of Bangladesh</li>
+<li> Least delivery charge</li>
+<li> Original Product</li>
+<li> Quality guaranteed</li>
+</ul>
+</p>
+
+</body>
+</html>
