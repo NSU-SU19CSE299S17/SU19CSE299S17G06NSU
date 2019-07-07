8b72cacbcb226f33c762152220db160387750632
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-07 23:18:37 +0600

    issues #19 Basic structure created

---
+++ Project Code/homepage/signup.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sign Up</title>
+<link href="signup.css" rel="stylesheet" type="text/css"
+</head>
+<body>
+ <div class="signup-page">
+ 	
+	<p class="message"> Already Registered? <a href="login.php"> Log In </a> </p>
+	</form>
+	</body>
+</html>
