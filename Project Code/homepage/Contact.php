26cae711372738bb5f8765dd947d6d31033c973f
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-07 22:43:45 +0600

    issues #13 contact page added

---
+++ Project Code/homepage/Contact.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+</head>
+<body>
+<style>
+
+<link href="style.css" rel="stylesheet" type="text/css">
+<p>
+
+<ul>
+<li> Telephone: +88028963098 </li>
+<li> Cellphone: +8801710000000-5 </li>
+<li> Facebook: fb/FurnitureOnline</li>
+<li> Twitter: @FurnitureOnline</li>
+<li> LinkedIn: LinkedIn/FurnitureOnline</li>
+</ul>
+
+For any inquiry, please <a href="mailto:FurnitureOnline@gmail.com">Mail</a> us. 
+</p>
+
+
+
+
+
+</p> </h3>
+
+</p>
+</body>
+</html>
