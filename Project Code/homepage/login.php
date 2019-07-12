dc35185132d13cfcd9b95c7d5fbcd8df059e07a8
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-12 23:50:39 +0600

    Issues #20 : Form for Logging In

--- Project Code/homepage/login.php
+++ Project Code/homepage/login.php
@@ -6,6 +6,14 @@
 </head>
 <body>
  <div class="login-page">
-
+ <div class="form">
+	<form class="login-form" method="POST" >
+	<input type="text" name="name" placeholder="User Name"/>
+	<input type="password"name="password" placeholder="Password"/>
+	
+	<input type="submit" class="button"  name="login" value="login"/>
+	
+	<p class="message"> Not Registered? <a href="signup.php"> Sign Up </a> </p>
+	</form>
 	</body>
 </html>
