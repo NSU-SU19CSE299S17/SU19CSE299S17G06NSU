a459504c062c49e5108e16013d38805a72a6bd8a
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-22 10:13:55 +0600

    Feature #27 : Matching Data and Sending User to Post Page

--- Project Code/homepage/login.php
+++ Project Code/homepage/login.php
@@ -12,44 +12,53 @@
 	<input type="password"name="password" placeholder="Password"/>
 	
 	<input type="submit" class="button"  name="login" value="login"/>
-
-<?php
+	
+	
+	<?php
 	
 	session_start();
 	
 	if (isset($_POST['login'])){
 	include_once("config.php");
-
-	$name = mysqli_real_escape_string($conn, $_POST['name']);
+		
+		$name = mysqli_real_escape_string($conn, $_POST['name']);
 		$password = mysqli_real_escape_string($conn, $_POST['password']);
 		
 		if(empty($name)||empty($password)){
 			header("Location:index.php?login=empty");
 				exit();
-			} 
-			else{
+			
+		} else{
 			$sql = "SELECT*FROM users where Name='$name'";
 			$result = mysqli_query($conn,$sql);
 			$resultCheck = mysqli_num_rows($result);
 			if($resultCheck<1){
 				header("Location:index.php?login=error");
-			exit();
-			} 
-			else{
+				exit();
+			} else{
 				if($row = mysqli_fetch_assoc($result)){
 					$hashedPwdCheck = Password_verify($password, $row['Password']);
 					if($hashedPwdCheck == false){
 						header("Location:index.php?login=error");
 				exit();
 					}
-
-
-
-}
-
+					elseif($hashedPwdCheck==true){
+						
+						$_SESSION['name'] = $row['Name'];
+						$_SESSION['password'] = $row['Password'];
+						$_SESSION['email'] = $row['Email'];
+						$_SESSION['number'] = $row['Number'];
+						$_SESSION['location'] = $row['Location'];
+						header("Location:post_page.php?login=success");
+						exit();	
+					}				
+				}
+			}
+		}
+		
+	}
+		
 ?>
-
-	
 	<p class="message"> Not Registered? <a href="signup.php"> Sign Up </a> </p>
 	</form>
 	</body>
