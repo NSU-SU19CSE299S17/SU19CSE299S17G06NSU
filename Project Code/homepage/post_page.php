0916cb2c61f0fa21e4e7f76230daae75b1b52142
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-28 22:52:17 +0600

    Feature #25 : Code structure fixed

--- Project Code/homepage/post_page.php
+++ Project Code/homepage/post_page.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
-
 <html>
 <head>
 <title>Image Upload</title>
-
-
 <style type="text/css">
 	*
-	{
+
+{
 	background-image:url(image/welcome.jpg);
 	
 	background-size:cover;
 	background-position:center;
 	font-size:15px;
 	}
-	 #content{
+   #content{
    	width: 70%;
    	margin: 20px auto;
    	border: 1px solid #cbcbcb;
@@ -26,28 +24,24 @@
    	margin: 20px auto;
 	font-size:20px;
    }
-
-    form div{
+   form div{
    	margin-top: 5px;
 	font-size:20px;
 	
    	background-color: white;
    	
    }
-
    #img_div{
    	width: 80%;
    	padding: 5px;
    	margin: 15px auto;
    	border: 1px solid #cbcbcb;
    }
-
    #img_div:after{
    	content: "";
    	display: block;
    	clear: both;
    }
-
    img{
    	float: left;
    	margin: 5px;
@@ -55,15 +49,12 @@
    	height: 300px;
    	display: block;
    }
-
    textarea{
    	background-color: white;
    }
 </style>
-
 </head>
 <body>
-
 <div id="content">
   <?php
     while ($row = mysqli_fetch_array($result)) {
@@ -73,14 +64,12 @@
       echo "</div>";
     }
   ?>
-
   <form method="POST" action="post_page.php" enctype="multipart/form-data">
-
-  <input type="hidden" name="size" value="1000000">
+  	<input type="hidden" name="size" value="1000000">
   	<div>
   	  <input type="file" name="image">
   	</div>
-<div>
+  	<div>
       <textarea 
       	id="text" 
       	cols="40" 
@@ -88,10 +77,10 @@
       	name="text" 
       	placeholder="Say something about your sell post and add description"></textarea>
   	</div>
-
   	<div>
   		<button type="submit" name="upload">POST</button>
   	</div>
-
+  </form>
+</div>
 </body>
 </html>
