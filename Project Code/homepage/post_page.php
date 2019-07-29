9bc6ad86b5ab17ee04019f7c28a62674026aa462
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-07-29 10:14:46 +0600

    Feature #36 : Image and Description selesction

--- Project Code/homepage/post_page.php
+++ Project Code/homepage/post_page.php
@@ -2,7 +2,16 @@
   
   $db = mysqli_connect("localhost", "root", "", "project");
 
+$msg = "";
 
+  
+  if (isset($_POST['upload'])) {
+  	
+  	$image = $_FILES['image']['name'];
+  	
+  	$text = mysqli_real_escape_string($db, $_POST['text']);
+
+  	
 
 
 
