9bc7734a2c91881446b19401b2fe2fe9c8a4ffce
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-08-04 23:59:27 +0600

    Feature #39 : Background image added

---
+++ Project Code/homepage/Client.php
@@ -0,0 +1,48 @@
+<?php
+  
+  $db = mysqli_connect("localhost", "root", "", "project");
+
+
+  $msg = "";
+
+  
+  if (isset($_POST['upload'])) {
+  	
+  	$image = $_FILES['image']['name'];
+  	
+  	$text = mysqli_real_escape_string($db, $_POST['text']);
+
+  	
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, text) VALUES ('$image', '$text')";
+  	
+  	mysqli_query($db, $sql);
+
+  	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+  		$msg = "Image uploaded successfully";
+  	}else{
+  		$msg = "Failed to upload image";
+  	}
+  }
+  $result = mysqli_query($db, "SELECT * FROM images");
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Image Upload</title>
+<style type="text/css">
+	*
+{
+	background-image:url(image/welcome.jpg);
+	
+	background-size:cover;
+	background-position:center;
+	font-size:15px;
+	}
+
+
+</body>
+</html>
