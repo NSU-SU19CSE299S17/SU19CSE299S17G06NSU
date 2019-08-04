bc701b35b581cf010a8186f8a1b5155a8230f3f5
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-08-04 23:52:39 +0600

    Feature #38 : Pushing values in database

--- Project Code/homepage/Client.php
+++ Project Code/homepage/Client.php
@@ -11,6 +11,12 @@
   	$image = $_FILES['image']['name'];
   	
   	$text = mysqli_real_escape_string($db, $_POST['text']);
+
+  	$target = "images/".basename($image);
+
+  	$sql = "INSERT INTO images (image, text) VALUES ('$image', '$text')";
+  	
+  	mysqli_query($db, $sql);
 ?>
 
 
