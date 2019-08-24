bc0565615e069ae2445f203c7fe18c6430a6b14f
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-08-24 20:37:57 +0600

    Feature #45 : Embed Code added from google map

--- Project Code/homepage/Contact.php
+++ Project Code/homepage/Contact.php
@@ -18,5 +18,7 @@
 <br>
 </p>
 
+<p align="center"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3650.1670244504917!2d90.42269671370538!3d23.81265900833819!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c64d82f2aff5%3A0x4064dbf4ade56307!2sJamuna%20Amusement%20Park!5e0!3m2!1sen!2sbd!4v1566656685375!5m2!1sen!2sbd" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></p>
+
 </body>
 </html>
