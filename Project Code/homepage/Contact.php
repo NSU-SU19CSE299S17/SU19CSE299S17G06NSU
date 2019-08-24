dad9671c3760ff298dc67c69353feb7ffbc99b1e
Faysalahmed1 <faysal.ahmed03@northsouth.edu>
2019-08-24 20:35:57 +0600

    Feature #45 : Texts added nd alligned

--- Project Code/homepage/Contact.php
+++ Project Code/homepage/Contact.php
@@ -2,7 +2,21 @@
 <html>
 <head>
 	<title> Contact Us </title>
-	
+
 </head>
+
+<body>
+
+<h1 align="center">
+	Contact info
+</h1>
+
+<p align="center">
+	
+<strong> Address : Jamuna Future park </strong>
+<br> Cell number : 99999999999
+<br>
+</p>
+
 </body>
 </html>
